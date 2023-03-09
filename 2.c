9e237973f2d99e9989a6442f414471a2f042b703
Даниил Истомин <58966238+codbid@users.noreply.github.com>
2023-03-10 01:07:09 +0700

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+ 
+typedef struct 
+{
+    int num;
+    char str[3];  
+} NumberRepr; 
+
+void format(NumberRepr* number) 
+{ 
+    sprintf(number->str, "%3d", number->num); 
+} 
+
+int main() 
+{ 
+    NumberRepr number = { .num = 1025 }; 
+    format(&number); 
+    printf("str: %s\n", number.str); 
+    printf("num: %d\n", number.num); 
+    return 0; 
+}
