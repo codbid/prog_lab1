e188e7afcbab83c9a70ca9f55752ca54a2e2aea6
Даниил Истомин <58966238+codbid@users.noreply.github.com>
2023-03-17 13:53:08 +0700

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,26 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-void init(int** arr, int n) 
+#include<stdlib.h>
+void init(int **arr, int n) 
 {
-    int *ar = malloc(n * sizeof(int));
-    int i;
-    for (i = 0; i < n; ++i) 
-    { 
-        ar[i] = i; 
-    }
-    *arr = ar; 
-}
- 
+	*arr = malloc(n * sizeof(int));
+	int i;
+	for (i = 0; i < n; ++i) 
+	{ 
+	(*arr)[i] = i; 
+	}
+} 
 int main() 
 { 
-    int* arr = NULL; 
-    int n = 10; 
-    init(&arr, n); 
-    int i; 
-    for (i = 0; i < n; ++i) 
-    { 
-        printf("%d\n", arr[i]); 
-    }  
-    return 0; 
+	int *arr = NULL; 
+	int n = 10; 
+	init(&arr, n); 
+	int i; 
+	for (i = 0; i < n; ++i) 
+	{ 
+	printf("%d\n", arr[i]); 
+	} 
+	return 0; 
 }
